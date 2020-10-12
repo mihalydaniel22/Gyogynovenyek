01ba3ece1b2b70424d7b1d30d81b98593bf53826
mihalydaniel22 <rikkiardo3@gmail.com>
2020-10-12 12:54:25 +0200

    Add project files.

---
+++ Gyogynovenyek/Megoldas.md
@@ -0,0 +1,61 @@
+﻿
+# Megoldás
+
+# Osztály létrehozása
+  - Kétféle mód közül bármelyik
+	- Ctrl + .
+	- Adjunk hozzá egy osztályt
+  - 5 tulajdonság
+	- public get, private set
+  - 1 konstruktor "?"
+	- 1 paraméter ---> string
+	- például "Acsalapu;levél;6;8"
+	- ';'-vel vannak elválasztva
+	- 4 darab adat ---> 5 tulajdonság??
+	- átadott, fájlból beolvasott sort:
+		- ';' mentén tagoljuk
+		- [0] ---> Nev
+		- [1] ---> Resz
+		- [2] ---> Kezd
+		- [3] ---> Veg
+		- Idotartam ---> számolás (lásd következő pont)
+  - 5. tulajdonság --> időtartam
+		- Konstruktorban számoljuk
+		- számolása: Vég - Kezdet HA!! Vég > Kezdet
+		- Vég > Kezdet Vég - Kezdet + 1
+		- Egyébként ---> 12 - Vég + Kezdet + 1
+
+ ##Adatok beolvasása és eltárolása
+	- Listában tároljuk az objektum példányokat
+	- Olvassuk be:
+		- System.IO
+		- StreamReader
+		- while ciklussal --> amíg tart a fájl
+		- Cikluson belül listához hozzáaunk egy új objektum példányt (beolvasott sor a paraméter)
+
+
+ ## 1. Feladat
+	- Hány növény van (lista Count tulajdonság)
+
+ ## 2. Feladat
+	- Milyen gyűjthető részek vannak?
+	- Hány darab van ezekből?
+	- Adatszerkezet: Dictionary<string, int>
+	- Milyen gyűjthető részek vannak
+		- Foreach "novenyek"
+		- Ha a rész nem kulcs ---> akkor adjuk hozzá 0-val
+	-[2] Hány darab van ezekből 
+		- Foreach "novenyek"
+		- Ha "Resz" == Key ---> Value++ 
+
+ ## 3. Feladat
+	- Maximum keresés ---> időtartam alapján
+	- Növény(ek) amely(ek) a legtöbb ideig szedtek
+
+ ## 4. Feladat
+	- Időtartam átlag számítás 
+		- szum változó = 0 (double)
+		- átlag változo
+		- foreach novenyek
+		- sum += Idotartam
+		- atlag = szum / count
---
+++ Gyogynovenyek/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gyogynovenyek
+{
+    class Program
+    {
+        static List<Noveny> novenyek = new List<Noveny>();
+        static Dictionary<string, int> reszek = new Dictionary<string, int>();
+
+        static void Beolvas()
+        {
+            StreamReader file = new StreamReader("noveny.csv");
+            while (!file.EndOfStream)
+            {
+                novenyek.Add(new Noveny(file.ReadLine()));
+            }
+            file.Close();
+        }
+        static void Elsofeladat()
+        {
+            Console.WriteLine($"1.Feladat: Növények Száma: {novenyek.Count}");
+        }
+
+        static void Masodikfeladat()
+        {
+            Console.WriteLine("2.Feladat: Gyűjthető részek:");
+            foreach (var n in novenyek)
+            {
+                if (!reszek.ContainsKey(n.Resz))
+                {
+                    reszek.Add(n.Resz, 0);
+                }
+            }
+
+            foreach (var n in novenyek)
+            {
+                reszek[n.Resz]++;
+            }
+
+            foreach (var r in reszek)
+            {
+                Console.WriteLine($"{r.Key}: {r.Value}");
+            }
+        }
+        static void Harmadikfeladat()
+        {
+            int max = 0;
+            string nev = "";
+            foreach (var n in novenyek)
+            {
+                if (n.Idotartam > max)
+                {
+                    max = n.Idotartam;
+                    nev = n.Nev;
+                }
+            }
+            Console.WriteLine("A {0} nevű növényt szedték a legtovább, {1}-hónapig",nev, max);
+        }
+        static void Negyedikfeladat()
+        {
+            double szum = 0;
+            double atlag = 0;
+            foreach (var n in novenyek)
+            {
+                szum += n.Idotartam;
+            }
+            atlag = szum / novenyek.Count;
+            Console.WriteLine("Átlagos gyűjthető időtartam {0}",atlag);
+        }
+
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Elsofeladat();
+            Masodikfeladat();
+            Harmadikfeladat();
+            Negyedikfeladat();
+            Console.ReadKey();
+        }
+    }
+}
